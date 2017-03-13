a7c94fb558642d720ebf49799ea830745846fb2b
Zachary Wander <zachary@techyteen.tk>
2017-03-13 17:29:33 -0400

    Derps fixed

--- app/src/main/java/zacharee1/com/modinstaller/MainActivity.java
+++ app/src/main/java/zacharee1/com/modinstaller/MainActivity.java
@@ -286,15 +286,15 @@ public class MainActivity extends AppCompatActivity
         });
     }
 
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
-    }
+//    @Override
+//    public void onBackPressed() {
+//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+//        if (drawer.isDrawerOpen(GravityCompat.START)) {
+//            drawer.closeDrawer(GravityCompat.START);
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
 
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
